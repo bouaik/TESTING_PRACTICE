f2092d87e1fcb7d9fde7966f9ef818395248bb27
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-20 00:53:54 +0100

    Create reverse string and test it

--- src/capitalized.js
+++ src/capitalized.js
@@ -2,7 +2,5 @@ const capitalized = (str) => {
     const firstLetter = str[0].toUpperCase();
     return firstLetter + str.slice(1);
   }
-  
-console.log(capitalized('start'))
 
 export default capitalized 
