2fb2ab51c69f073acf9f8b5ba8ab0d3e1a12a8d1
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-20 01:05:41 +0100

    Create add method in calc and test it

---
+++ src/calculator.js
@@ -0,0 +1,7 @@
+class Calculator {
+    add(a, b) {
+        return a + b
+    }
+}
+
+export default Calculator
---
+++ test/calculator.test.js
@@ -0,0 +1,6 @@
+import Calculator from '../src/calculator'
+
+test('adds 1 + 2 to equal 3', () => {
+    const result = new Calculator
+    expect(result.add(1, 2)).toBe(3);
+  });
