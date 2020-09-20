5e0a1c53a27b09150835c1a0370f884b8119462f
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-20 01:09:00 +0100

    Crate substarct method in calc and test it

--- src/calculator.js
+++ src/calculator.js
@@ -2,6 +2,11 @@ class Calculator {
     add(a, b) {
         return a + b
     }
+
+    substract(a, b) {
+        return a - b
+    }
+
 }
 
 export default Calculator
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,4 +3,9 @@ import Calculator from '../src/calculator'
 test('adds 1 + 2 to equal 3', () => {
     const result = new Calculator
     expect(result.add(1, 2)).toBe(3);
-  });
+});
+
+test('adds 1 + 2 to equal 3', () => {
+    const result = new Calculator
+    expect(result.substract(1, 2)).toBe(-1);
+});
